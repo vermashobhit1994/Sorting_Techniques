03e3ebda8eef968abb7cae53a9783cf95554f3aa
shobhit <=>
2021-01-11 15:53:08 +0530

    added the entry in headefile and Makefile for mergesort

--- header_array.h
+++ header_array.h
@@ -5,4 +5,5 @@ void swap_arr(int *pfirst,int *psecond);
 void display_arr(int *arr, int n);
 void bubble_sort_arr(int *arr,int n);
 void insertion_sort_arr(int *arr ,int n);
+void mergesort_arr(int *arr,int n);
 
